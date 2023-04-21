d490933cf1ba73597ed6015a547bb93e26950bd3
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-21 11:43:25 -0400

    Cleanup AllProducts.jsx component

--- frontend/src/components/AllProducts.jsx
+++ frontend/src/components/AllProducts.jsx
@@ -1,39 +1,32 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
 import { useGetAllProductsQuery } from "../features/productsApi";
-import {
-  fetchProductsAsync,
-  selectAllProducts,
-} from "../features/productsSlice";
 
 const AllProducts = () => {
-  // const dispatch = useDispatch();
-  // const allProducts = useSelector(selectAllProducts);
-  // const allProducts = useSelector((state) => state.products);
-
-  // useEffect(() => {
-  //   dispatch(fetchProductsAsync());
-  // }, [dispatch]);
-
-  // console.log("products", allProducts);
-  // console.log("items", allProducts.items);
-
   const { data: allProducts, error, isLoading } = useGetAllProductsQuery();
-  console.log("query", allProducts);
+  // console.log("query", allProducts);
   return (
     <>
       <h1>All Products</h1>
       <div>
-        {allProducts &&
-          allProducts.map((product) => {
-            return (
-              <div key={product.id}>
-                <h2>{product.name}</h2>
-                <img src={product.image} style={{ width: "300px" }} />
-              </div>
-            );
-          })}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>An error occured</p>
+        ) : (
+          <>
+            {allProducts?.map((product) => {
+              return (
+                <div key={product.id}>
+                  <h2>{product.name}</h2>
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    style={{ width: "300px" }}
+                  />
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
     </>
   );
