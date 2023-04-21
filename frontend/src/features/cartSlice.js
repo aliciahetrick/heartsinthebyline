ccee36e1f3952a269b60ceae4b8d93aa4dae49e8
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-21 17:32:14 -0400

    Rename initial state variables

--- frontend/src/features/cartSlice.js
+++ frontend/src/features/cartSlice.js
@@ -4,8 +4,8 @@ const initialState = {
   cartItems: localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [],
-  //   cartTotalQuantity: 0,
-  //   cartTotalAmount: 0,
+  cartTotalQuantity: 0,
+  cartTotalPrice: 0,
 };
 
 const cartSlice = createSlice({
@@ -69,7 +69,7 @@ const cartSlice = createSlice({
         }
       );
 
-      state.cartTotalQty = quantity;
+      state.cartTotalQuantity = quantity;
       state.cartTotalPrice = total;
     },
   },
