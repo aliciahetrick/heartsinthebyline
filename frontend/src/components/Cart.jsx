08e2a5cec28a0c1782e48333dcd5c8ab0c32504e
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-21 15:58:06 -0400

    Render cart items

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,5 +1,51 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 const Cart = () => {
-  return <h2>Cart</h2>;
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+  return (
+    <>
+      <h2>Cart</h2>
+      {cart.cartItems.length === 0 ? (
+        <div>
+          <p>Your cart is empty</p>
+          <div>
+            <Link to="/products">Start shopping</Link>
+          </div>
+        </div>
+      ) : (
+        <div>
+          {cart.cartItems.map((cartItem) => {
+            return (
+              <div key={cartItem.id}>
+                <div>
+                  <img
+                    src={cartItem.image}
+                    alt={cartItem.name}
+                    style={{ width: "300px" }}
+                  />
+                  <p>{cartItem.name}</p>
+                  <p>${cartItem.price} each</p>
+                  <button>-</button>
+                  <div>{cartItem.cartQty}</div>
+                  <button>+</button>
+                  <p>Total: ${cartItem.price * cartItem.cartQty}</p>
+                  <button>Remove</button>
+                </div>
+              </div>
+            );
+          })}
+          <div>
+            <button>Clear cart</button>
+            <p>Taxes and shipping calculated at checkout</p>
+            <p>Subtotal: ${cart.cartTotalPrice}</p>
+            <button>Checkout</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Cart;
