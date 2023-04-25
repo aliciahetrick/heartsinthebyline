150ed897b6d7808aaddd38991f3105efa5074bff
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-25 10:00:48 -0400

    Add cart data to stripe checkout page

--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -8,19 +8,27 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const router = express.Router();
 
 router.post("/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          //   description: item.desc,
+          metadata: {
+            id: item.id,
           },
-          unit_amount: 2000,
         },
-        quantity: 1,
+        unit_amount: item.price * 100,
       },
-    ],
+      quantity: item.cartQty,
+      //   quantity: item.cartQuant,
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
     cancel_url: `${process.env.CLIENT_URL}/cart`,
