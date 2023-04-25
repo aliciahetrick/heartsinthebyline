429e7341a9d98218dc653695583c42d21ed45bdf
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-25 10:18:46 -0400

    Add shipping to stripe payment page

--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -23,11 +23,80 @@ router.post("/create-checkout-session", async (req, res) => {
         unit_amount: item.price * 100,
       },
       quantity: item.cartQty,
-      //   quantity: item.cartQuant,
     };
   });
 
   const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: {
+      allowed_countries: ["US"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 500,
+            currency: "usd",
+          },
+          display_name: "USPS First-Class Mail",
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 5,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 1000,
+            currency: "usd",
+          },
+          display_name: "USPS Priority Mail",
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 5,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 3500,
+            currency: "usd",
+          },
+          display_name: "USPS Priority Mail Express",
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 2,
+            },
+          },
+        },
+      },
+    ],
+    // phone_number_collection: {
+    //   enabled: true,
+    // },
     line_items,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
