f68655cd7107018403485e0a159b273578ed0838
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-26 10:08:24 -0400

    Able to create a new product from the admin dashboard and persist to db

---
+++ backend/routes/products.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Product = require("../models/product");
+const cloudinary = require("../utils/cloudinary");
+
+const router = express.Router();
+
+// Fetch all products in MongoDB
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).send(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+// Create a product
+router.post("/", async (req, res) => {
+  const { name, desc, price, image } = req.body;
+
+  try {
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image, {
+        upload_preset: "hearts",
+      });
+
+      if (uploadResponse) {
+        const product = new Product({
+          name,
+          desc,
+          price,
+          image: uploadResponse,
+        });
+
+        const savedProduct = await product.save();
+        res.status(200).send(savedProduct);
+      }
+    }
+  } catch (err) {
+    console.log("backend error", err);
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
--- frontend/src/features/productsSlice.js
+++ frontend/src/features/productsSlice.js
@@ -1,9 +1,11 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { url } from "./api";
 
 const initialState = {
   items: [],
   status: null,
+  createStatus: null,
 };
 
 // export const fetchProductsAsync = createAsyncThunk(
@@ -19,6 +21,24 @@ const initialState = {
 //   }
 // );
 
+export const createProductAsync = createAsyncThunk(
+  "products/createProductAsync",
+  async (values) => {
+    console.log("values", values);
+    try {
+      const response = await axios.post(
+        `${url}/products`,
+        // "http://localhost:5000/api/products",
+        values
+      );
+      console.log("fetch response", response);
+      return response.data;
+    } catch (error) {
+      console.log("post error", error);
+    }
+  }
+);
+
 const productsSlice = createSlice({
   name: "products",
   initialState,
@@ -30,6 +50,19 @@ const productsSlice = createSlice({
   //       return {
   //         status: "pending",
   //         items: state.items,
+  extraReducers: (builder) => {
+    builder.addCase(createProductAsync.pending, (state, action) => {
+      state.createStatus = "pending";
+    });
+    builder.addCase(createProductAsync.fulfilled, (state, action) => {
+      state.items.push(action.payload);
+      state.createStatus = "success";
+    });
+    builder.addCase(createProductAsync.rejected, (state, action) => {
+      state.createStatus = "rejected";
+      console.log("rejected", action.error);
+    });
+  },
 });
 
 export const selectAllProducts = (state) => {
