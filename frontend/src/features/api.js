f9b6da810b7feff44305091872d7f22293927d67
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-27 11:06:54 -0400

    Add backend api route for deployment

--- frontend/src/features/api.js
+++ frontend/src/features/api.js
@@ -1,4 +1,5 @@
-export const url = "http://localhost:5000/api";
+// export const url = "http://localhost:5000/api";
+export const url = "https://heartsinthebyline.herokuapp.com//api";
 
 export const setHeaders = () => {
   const headers = {
