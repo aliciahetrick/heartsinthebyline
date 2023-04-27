10c271a59262f69c7e06c66e093180fec908e428
Alicia Hetrick <aliciahetrick@gmail.com>
2023-04-27 11:07:22 -0400

    Remove double / from path

--- frontend/src/features/api.js
+++ frontend/src/features/api.js
@@ -1,5 +1,5 @@
 // export const url = "http://localhost:5000/api";
-export const url = "https://heartsinthebyline.herokuapp.com//api";
+export const url = "https://heartsinthebyline.herokuapp.com/api";
 
 export const setHeaders = () => {
   const headers = {
