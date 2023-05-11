de00620c5aaea26d73fddac6d56753b52ac4d05c
Alicia Hetrick <aliciahetrick@gmail.com>
2023-05-10 21:27:40 -0400

    Start styling cart mobile view

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -10,6 +10,9 @@ import {
 } from "../features/cartSlice";
 import PayButton from "./PayButton";
 
+import styled from "styled-components";
+import { BREAKPOINTS } from "../constants/breakpoints";
+
 const Cart = () => {
   const dispatch = useDispatch();
 
@@ -38,7 +41,7 @@ const Cart = () => {
 
   return (
     <>
-      <h2>Cart</h2>
+      <WrapperTitle>Cart</WrapperTitle>
       {cart.cartItems.length === 0 ? (
         <div>
           <p>Your cart is empty</p>
@@ -47,45 +50,149 @@ const Cart = () => {
           </div>
         </div>
       ) : (
-        <div>
-          {cart.cartItems.map((cartItem) => {
-            return (
-              <div key={cartItem._id}>
-                <div>
-                  <Link to={`/products/${cartItem.url}`}>
-                    <img
-                      src={cartItem.image.url}
-                      alt={cartItem.name}
-                      style={{ width: "300px" }}
-                    />
-                    <p>{cartItem.name}</p>
-                  </Link>
-                  <p>${cartItem.price} each</p>
-                  <button onClick={() => handleDecreaseCartQuantity(cartItem)}>
-                    -
-                  </button>
-                  <div>{cartItem.cartQty}</div>
-                  <button onClick={() => handleIncreaseCartQuantity(cartItem)}>
-                    +
-                  </button>
-                  <p>Total: ${cartItem.price * cartItem.cartQty}</p>
-                  <button onClick={() => handleRemoveFromCart(cartItem)}>
-                    Remove
-                  </button>
-                </div>
-              </div>
-            );
-          })}
+        <>
+          <AllCartItemsContainer>
+            {cart.cartItems.map((cartItem) => {
+              return (
+                <CartItemContainer key={cartItem._id}>
+                  <div>
+                    <Link
+                      to={`/products/${cartItem.url}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <CartItemImage
+                        src={cartItem.image.url}
+                        alt={cartItem.name}
+                        // style={{ width: "300px" }}
+                      />
+                      <CartItemName>{cartItem.name}</CartItemName>
+                    </Link>
+                    <p>${cartItem.price} each</p>
+                    <CartQuantityButtonContainer>
+                      <CartQuantityButtonMinus
+                        onClick={() => handleDecreaseCartQuantity(cartItem)}
+                      >
+                        -
+                      </CartQuantityButtonMinus>
+                      <CartQuantityNumber>
+                        {cartItem.cartQty}
+                      </CartQuantityNumber>
+                      <CartQuantityButtonPlus
+                        onClick={() => handleIncreaseCartQuantity(cartItem)}
+                      >
+                        +
+                      </CartQuantityButtonPlus>
+                    </CartQuantityButtonContainer>
+                    <p>Total: ${cartItem.price * cartItem.cartQty}</p>
+                    <button onClick={() => handleRemoveFromCart(cartItem)}>
+                      Remove
+                    </button>
+                  </div>
+                </CartItemContainer>
+              );
+            })}
+          </AllCartItemsContainer>
           <div>
             <button onClick={() => handleClearCart()}>Clear cart</button>
             <p>Taxes and shipping calculated at checkout</p>
             <p>Subtotal: ${cart.cartTotalPrice}</p>
             <PayButton cartItems={cart.cartItems} />
           </div>
-        </div>
+        </>
       )}
     </>
   );
 };
 
 export default Cart;
+
+const WrapperTitle = styled.h1`
+  color: #f578a6;
+  text-align: center;
+  font-size: 1.5rem;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
+  margin-top: 1em;
+  margin-bottom: 1em;
+`;
+
+const AllCartItemsContainer = styled.div`
+  @media only screen and (min-width: ${BREAKPOINTS.medium}) {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 1em;
+    flex-wrap: wrap;
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+`;
+
+const CartItemContainer = styled.div`
+  padding-bottom: 3em;
+  // border-bottom: 3px solid pink;
+  &::after {
+    content: "";
+    position: absolute;
+    margin: auto;
+    margin-top: 1em;
+    right: 0;
+    left: 0;
+    width: 50%;
+    height: 2px;
+    background-color: pink;
+  }
+`;
+
+const CartItemImage = styled.img`
+  width: 250px;
+  border-radius: 1em;
+  @media only screen and (min-width: ${BREAKPOINTS.medium}) {
+    width: 300px;
+  }
+`;
+
+const CartItemName = styled.h2`
+  color: #f578a6;
+  text-align: center;
+  font-size: 1rem;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
+  margin-top: 0.5em;
+  @media only screen and (min-width: ${BREAKPOINTS.medium}) {
+    max-width: 300px;
+  }
+`;
+
+const CartQuantityButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CartQuantityButtonMinus = styled.button`
+  background-color: #f578a6;
+  color: white;
+  padding: 0.5em;
+  border-top-left-radius: 1em;
+  border-bottom-left-radius: 1em;
+  border: none;
+  padding-left: 1em;
+`;
+
+const CartQuantityNumber = styled.div`
+  background-color: #f578a6;
+  color: white;
+  padding: 0.5em;
+  padding-left: 1em;
+  padding-right: 1em;
+`;
+
+const CartQuantityButtonPlus = styled.button`
+  background-color: #f578a6;
+  color: white;
+  padding: 0.5em;
+  border-top-right-radius: 1em;
+  border-bottom-right-radius: 1em;
+  border: none;
+  padding-right: 1em;
+`;
