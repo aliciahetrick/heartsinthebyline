6f70f2d621cf2a89a789fda020287397fbc2a8fc
Alicia Hetrick <aliciahetrick@gmail.com>
2023-05-11 18:20:02 -0400

    Add cart items flexbox containers

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -52,11 +52,16 @@ const Cart = () => {
       ) : (
         <>
           <AllCartItemsContainer>
+            <HeadingWrapper>
+              <ProductWrapper>Product</ProductWrapper>
+              <QuantityWrapper>Quantity</QuantityWrapper>
+              <TotalWrapper>Total</TotalWrapper>
+            </HeadingWrapper>
             {cart.cartItems.map((cartItem) => {
               return (
                 <CartItemContainer key={cartItem._id}>
-                  <div>
-                    <Link
+                  <>
+                    <CartItemLeftWrapper
                       to={`/products/${cartItem.url}`}
                       style={{ textDecoration: "none" }}
                     >
@@ -65,29 +70,39 @@ const Cart = () => {
                         alt={cartItem.name}
                         // style={{ width: "300px" }}
                       />
-                      <CartItemName>{cartItem.name}</CartItemName>
-                    </Link>
-                    <p>${cartItem.price} each</p>
-                    <CartQuantityButtonContainer>
-                      <CartQuantityButtonMinus
-                        onClick={() => handleDecreaseCartQuantity(cartItem)}
+                      <CartItemDetailsWrapper>
+                        <CartItemName>{cartItem.name}</CartItemName>
+                        <CartItemPrice>${cartItem.price} each</CartItemPrice>
+                      </CartItemDetailsWrapper>
+                    </CartItemLeftWrapper>
+                    <CartItemMiddleWrapper>
+                      <CartQuantityButtonContainer>
+                        <CartQuantityButtonMinus
+                          onClick={() => handleDecreaseCartQuantity(cartItem)}
+                        >
+                          -
+                        </CartQuantityButtonMinus>
+                        <CartQuantityNumber>
+                          {cartItem.cartQty}
+                        </CartQuantityNumber>
+                        <CartQuantityButtonPlus
+                          onClick={() => handleIncreaseCartQuantity(cartItem)}
+                        >
+                          +
+                        </CartQuantityButtonPlus>
+                      </CartQuantityButtonContainer>
+                      <RemoveCartItemButton
+                        onClick={() => handleRemoveFromCart(cartItem)}
                       >
-                        -
-                      </CartQuantityButtonMinus>
-                      <CartQuantityNumber>
-                        {cartItem.cartQty}
-                      </CartQuantityNumber>
-                      <CartQuantityButtonPlus
-                        onClick={() => handleIncreaseCartQuantity(cartItem)}
-                      >
-                        +
-                      </CartQuantityButtonPlus>
-                    </CartQuantityButtonContainer>
-                    <p>Total: ${cartItem.price * cartItem.cartQty}</p>
-                    <button onClick={() => handleRemoveFromCart(cartItem)}>
-                      Remove
-                    </button>
-                  </div>
+                        Remove
+                      </RemoveCartItemButton>
+                    </CartItemMiddleWrapper>
+                    <CartItemRightWrapper>
+                      <CartItemTotal>
+                        ${cartItem.price * cartItem.cartQty}
+                      </CartItemTotal>
+                    </CartItemRightWrapper>
+                  </>
                 </CartItemContainer>
               );
             })}
@@ -116,52 +131,124 @@ const WrapperTitle = styled.h1`
   margin-bottom: 1em;
 `;
 
-const AllCartItemsContainer = styled.div`
-  @media only screen and (min-width: ${BREAKPOINTS.medium}) {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    gap: 1em;
-    flex-wrap: wrap;
-    margin-left: 10%;
-    margin-right: 10%;
-  }
-`;
-
-const CartItemContainer = styled.div`
-  padding-bottom: 3em;
-  // border-bottom: 3px solid pink;
+const HeadingWrapper = styled.div`
+  display: flex;
+  margin-left: 10%;
+  margin-right: 10%;
+  // border: 1px solid red;
+  color: #f578a6;
+  text-align: center;
+  font-size: 0.5rem;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
   &::after {
     content: "";
     position: absolute;
     margin: auto;
-    margin-top: 1em;
-    right: 0;
-    left: 0;
-    width: 50%;
+    margin-top: 4em;
+    width: 80%;
     height: 2px;
     background-color: pink;
   }
 `;
 
+const ProductWrapper = styled.h2`
+  width: 60%;
+  text-align: left;
+  // border: 1px solid green;
+`;
+const QuantityWrapper = styled.h2`
+  width: 20%;
+  // border: 1px solid green;
+`;
+
+const TotalWrapper = styled.h2`
+  width: 20%;
+  // border: 1px solid green;
+`;
+
+const AllCartItemsContainer = styled.div`
+  @media only screen and (min-width: ${BREAKPOINTS.medium}) {
+    // display: flex;
+    // flex-direction: row;
+    // justify-content: center;
+    // gap: 1em;
+    // flex-wrap: wrap;
+    // margin-left: 10%;
+    // margin-right: 10%;
+  }
+`;
+
+const CartItemContainer = styled.div`
+  display: flex;
+  padding-bottom: 2em;
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 2em;
+
+  border-bottom: 2px solid pink;
+  // &::after {
+  //   content: "";
+  //   position: absolute;
+  //   margin: auto;
+  //   margin-top: 1em;
+  //   right: 0;
+  //   left: 0;
+  //   width: 80%;
+  //   height: 2px;
+  //   background-color: pink;
+  // }
+`;
+
+const CartItemLeftWrapper = styled(Link)`
+  width: 60%;
+  display: flex;
+`;
+
 const CartItemImage = styled.img`
-  width: 250px;
+  width: 120px;
+  height: 120px;
   border-radius: 1em;
   @media only screen and (min-width: ${BREAKPOINTS.medium}) {
     width: 300px;
+    height: 300px;
   }
+`;
+
+const CartItemDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 1em;
 `;
 
 const CartItemName = styled.h2`
   color: #f578a6;
-  text-align: center;
+  text-align: left;
   font-size: 1rem;
   font-family: "Raleway", sans-serif;
   text-transform: uppercase;
   margin-top: 0.5em;
-  @media only screen and (min-width: ${BREAKPOINTS.medium}) {
-    max-width: 300px;
-  }
+  // border: 1px solid blue;
+`;
+
+const CartItemPrice = styled.h2`
+  color: pink;
+  text-align: left;
+  font-size: 1rem;
+  font-weight: 600;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
+  margin-top: 0.5em;
+  // border: 1px solid blue;
+`;
+
+const CartItemMiddleWrapper = styled.div`
+  width: 20%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1em;
 `;
 
 const CartQuantityButtonContainer = styled.div`
@@ -181,6 +268,7 @@ const CartQuantityButtonMinus = styled.button`
 
 const CartQuantityNumber = styled.div`
   background-color: #f578a6;
+  font-family: "Raleway", sans-serif;
   color: white;
   padding: 0.5em;
   padding-left: 1em;
@@ -195,4 +283,39 @@ const CartQuantityButtonPlus = styled.button`
   border-bottom-right-radius: 1em;
   border: none;
   padding-right: 1em;
+`;
+
+const RemoveCartItemButton = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  text-decoration: underline;
+
+  color: pink;
+  font-size: 1em;
+  font-weight: 600;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
+`;
+
+const CartItemRightWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // border: 1px solid green;
+  width: 20%;
+`;
+
+const CartItemTotal = styled.h2`
+  color: pink;
+  font-size: 1rem;
+  font-weight: 600;
+  font-family: "Raleway", sans-serif;
+  text-transform: uppercase;
+  margin-top: 0.5em;
+  // border: 1px solid blue;
 `;
