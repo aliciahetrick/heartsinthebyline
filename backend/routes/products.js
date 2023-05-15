e3e5a0f862f881c79cdb06fe92959782f13daa19
Alicia Hetrick <aliciahetrick@gmail.com>
2023-05-14 21:57:39 -0400

    Cleanup

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,75 +1,36 @@
 const express = require("express");
-const { isAdmin } = require("../middleware/auth");
-const Product = require("../models/product");
-const cloudinary = require("../utils/cloudinary");
 
 const router = express.Router();
 
-// Fetch all products in MongoDB
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+// Fetch all products from Stripe API
 router.get("/", async (req, res) => {
   try {
-    const products = await Product.find();
-    res.status(200).send(products);
+    const products = await stripe.products.list({
+      limit: 3,
+    });
+    res.send(products);
   } catch (err) {
     console.log(err);
     res.status(500).send(err);
   }
 });
 
-// Fetch single product in MongoDB
+// Fetch single product from Stripe API
 router.get("/:_id", async (req, res, next) => {
   try {
-    // console.log("requesrrrt", req.params._id);
-    const product = await Product.findOne({ url: req.params._id });
+    const product = await stripe.products.retrieve(req.params._id);
+    const priceOfProduct = product.default_price;
+
+    const price = await stripe.prices.retrieve(priceOfProduct);
+
+    product.price = price;
+
     res.status(200).send(product);
   } catch (err) {
     console.log(err);
-    res.status(500).send(err);
-  }
-});
-
-router.put("/:id", async (req, res, next) => {
-  try {
-    console.log(req.body);
-    const product = await Product.findOne({ url: req.params.id });
-    product.stock = product.stock - req.body.cartQty;
-    await product.save();
-    res.status(200).send(product);
-  } catch (err) {
-    console.log("Stock update error", err);
-
-    res.status(500).send({ error: "stock_error" });
-  }
-});
-
-// Create a product
-// router.post("/", async (req, res) => {
-router.post("/", isAdmin, async (req, res) => {
-  const { name, type, desc, price, image, url, stock } = req.body;
-
-  try {
-    if (image) {
-      const uploadResponse = await cloudinary.uploader.upload(image, {
-        upload_preset: "hearts",
-      });
-
-      if (uploadResponse) {
-        const product = new Product({
-          name,
-          type,
-          url,
-          desc,
-          price,
-          stock,
-          image: uploadResponse,
-        });
-
-        const savedProduct = await product.save();
-        res.status(200).send(savedProduct);
-      }
-    }
-  } catch (err) {
-    console.log("backend error", err);
     res.status(500).send(err);
   }
 });
