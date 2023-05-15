efc37b404f1981810f9dfaff77c8d24ba3702ac3
Alicia Hetrick <aliciahetrick@gmail.com>
2023-05-15 11:39:42 -0400

    Add post route to updatestock quantity in stripe API

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -19,14 +19,31 @@ router.get("/", async (req, res) => {
 });
 
 // Fetch single product from Stripe API
-router.get("/:_id", async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
-    const product = await stripe.products.retrieve(req.params._id);
+    const product = await stripe.products.retrieve(req.params.id);
     const priceOfProduct = product.default_price;
 
     const price = await stripe.prices.retrieve(priceOfProduct);
 
     product.price = price;
+
+    res.status(200).send(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+router.post("/:id", async (req, res, next) => {
+  try {
+    const { metadata } = await stripe.products.retrieve(req.params.id);
+    const metadataStock = await metadata.stock;
+    const product = await stripe.products.update(req.params.id, {
+      metadata: {
+        stock: Number(metadataStock) - req.body.purchasedQuantity,
+      },
+    });
 
     res.status(200).send(product);
   } catch (err) {
