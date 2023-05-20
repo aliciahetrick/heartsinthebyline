67a619c93a0a5d92975cd8b484636052c757032b
Alicia Hetrick <aliciahetrick@gmail.com>
2023-05-20 17:18:07 -0400

    Initialize postgres db connection

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+const productsRoute = require("./routes/products");
+
+// logging middleware
+app.use(morgan('dev'))
+
+// body parsing middleware
+app.use(express.json())
+
+// auth and api routes
+// app.use("/api/register", register);
+// app.use("/api/login", login);
+// app.use("/api/stripe", stripe);
+app.use("/api/products", productsRoute);
+
+app.get('/', (req, res) => {
+res.send("root")
+})
+
+module.exports = app
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,52 +1,104 @@
-const express = require("express");
+// const express = require("express");
 
-const router = express.Router();
+// const router = express.Router();
 
-const Stripe = require("stripe");
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+// const Stripe = require("stripe");
+// const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-// Fetch all products from Stripe API
-router.get("/", async (req, res) => {
+// // Fetch all products from Stripe API
+// router.get("/", async (req, res) => {
+//   try {
+//     const products = await stripe.products.list();
+//     res.send(products);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send(err);
+//   }
+// });
+
+// // Fetch single product from Stripe API
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const product = await stripe.products.retrieve(req.params.id);
+//     const priceOfProduct = product.default_price;
+
+//     const price = await stripe.prices.retrieve(priceOfProduct);
+
+//     product.price = price;
+
+//     res.status(200).send(product);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send(err);
+//   }
+// });
+
+// router.post("/:id", async (req, res, next) => {
+//   try {
+//     const { metadata } = await stripe.products.retrieve(req.params.id);
+//     const metadataStock = await metadata.stock;
+//     const product = await stripe.products.update(req.params.id, {
+//       metadata: {
+//         stock: Number(metadataStock) - req.body.purchasedQuantity,
+//       },
+//     });
+//     res.status(200).send(product);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send(err);
+//   }
+// });
+
+// module.exports = router;
+
+const router = require('express').Router()
+const {
+  models: { Product },
+} = require('../db')
+
+router.get('/', async (req, res, next) => {
   try {
-    const products = await stripe.products.list();
-    res.send(products);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
+    res.send(
+      await Product.findAll())
+  } catch (error) {
+    next(error)
   }
-});
+})
 
-// Fetch single product from Stripe API
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res) => {
   try {
-    const product = await stripe.products.retrieve(req.params.id);
-    const priceOfProduct = product.default_price;
-
-    const price = await stripe.prices.retrieve(priceOfProduct);
-
-    product.price = price;
-
-    res.status(200).send(product);
+    res.send(await Product.findByPk(req.params.id))
   } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
+    console.log('There was a problem fetching the product.', err)
   }
-});
+})
 
-router.post("/:id", async (req, res, next) => {
+router.post('/', async (req, res) => {
   try {
-    const { metadata } = await stripe.products.retrieve(req.params.id);
-    const metadataStock = await metadata.stock;
-    const product = await stripe.products.update(req.params.id, {
-      metadata: {
-        stock: Number(metadataStock) - req.body.purchasedQuantity,
-      },
-    });
-    res.status(200).send(product);
+    const product = await Product.create(req.body)
+    res.send(product)
   } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
+    console.log(err)
   }
-});
+})
 
-module.exports = router;
+router.put('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id)
+    res.send(await product.update(req.body))
+  } catch (err) {
+    console.log('There was a problem updating product.', err)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id)
+    await product.destroy()
+    res.send(product)
+  } catch (err) {
+    console.log('Could not delete!', err)
+  }
+})
+
+module.exports = router
