cf96be4aca43fbbb0deb1e21905cb8af59b2d7c3
Alicia Hetrick <aliciahetrick@gmail.com>
2023-05-24 14:43:58 -0400

    Update api url to use enviroment variable

--- frontend/src/features/api.js
+++ frontend/src/features/api.js
@@ -1,5 +1,5 @@
 // FOR DEVELOPMENT:
-export const url = "http://localhost:8080/api";
+export const url = process.env.REACT_APP_API_URL;
 
 // FOR PRODUCTION:
 // export const url = "https://heartsinthebyline.herokuapp.com/api";
