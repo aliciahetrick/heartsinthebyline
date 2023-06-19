350f8611a243f59df41e714742261f178869e14b
Alicia Hetrick <aliciahetrick@gmail.com>
2023-06-19 16:25:16 -0400

    Style 404 error page

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+
 const NotFound = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <Wrapper>
+      <Status>404</Status>
+      <ImageWrapper src="../../throwing-chair.gif" />
+      <Title>Page Not Found</Title>
+    </Wrapper>
+  );
 };
 
 export default NotFound;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 6em;
+  align-items: center;
+`;
+
+const Status = styled.h2`
+  text-align: center;
+  color: #f578a6;
+  font-family: Raleway;
+  font-weight: 800;
+  font-size: 50px;
+  text-transform: uppercase;
+`;
+
+const ImageWrapper = styled.img`
+  object-fit: cover;
+  width: 200px;
+  height: 200px;
+  border-radius: 1.5em;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  color: #f578a6;
+  font-family: Raleway;
+  font-weight: 800;
+  font-size: 20px;
+  margin-top: 2em;
+  text-transform: uppercase;
+`;
