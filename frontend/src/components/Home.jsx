46b7f0e460d56abe4ffdc90a15d441256a1a8761
Alicia Hetrick <aliciahetrick@gmail.com>
2023-07-05 16:21:25 -0400

    Add banner to homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <PageContainer>
+      {/* <h2>Home</h2> */}
+      {/* <Link
+        to={`/products`}
+        style={{
+          textDecoration: "none",
+          color: "#f578a6",
+          fontSize: "5em",
+        }}>
+        SHOP
+      </Link> */}
+      <ImageWrapper src="../../banner.jpeg" />
+    </PageContainer>
+  );
 };
 
 export default Home;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ImageWrapper = styled.img`
+  object-fit: contain;
+`;
