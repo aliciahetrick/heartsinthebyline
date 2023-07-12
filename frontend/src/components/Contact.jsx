acca551aa0ab9062e019fc5abe9b19912ab0ac65
Alicia Hetrick <aliciahetrick@gmail.com>
2023-07-12 19:04:35 -0400

    Add emailjs to contact view

---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        `${process.env.REACT_APP_EMAILJS_SERVICE_ID}`,
+        `${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`,
+        form.current,
+        // "YOUR_PUBLIC_KEY"
+        `${process.env.REACT_APP_EMAILJS_PUBLIC_KEY}`
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("Message sent");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default Contact;
